360fbe94386c206ca48818e682c978d3dc3f7ada
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-10-19 15:37:19 +0900

    feat: change navbar title

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from "@astrojs/starlight";
 export default defineConfig({
   integrations: [
     starlight({
-      title: "Nix Wiki",
+      title: "My Nix",
       social: {
         github: "https://github.com/Myxogastria0808/dotfiles",
       },
